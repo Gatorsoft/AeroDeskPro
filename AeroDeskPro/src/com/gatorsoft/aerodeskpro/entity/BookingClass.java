2f376c69287c9c0e3b0abfbd94f5eff3c4a77694
Mavros-Lykos <amarasekaracharaka2@gmail.com>
2025-07-20 02:15:31 +0530

    BookingClass Entity created and added for Enitity

---
+++ AeroDeskPro/src/com/gatorsoft/aerodeskpro/entity/BookingClass.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.gatorsoft.aerodeskpro.entity;
+
+    // ===== Enum for Booking Class =====
+    public enum BookingClass {
+        ECONOMY, BUSINESS, FIRST
+    }
