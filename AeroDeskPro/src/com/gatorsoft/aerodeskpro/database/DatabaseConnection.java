023d8e0634febfbf402fb28ba839e1ad04d0043c
Mavros-Lykos <amarasekaracharaka2@gmail.com>
2025-07-19 17:13:37 +0530

    Database added to the Aiven connection

--- AeroDeskPro/src/com/gatorsoft/aerodeskpro/database/DatabaseConnection.java
+++ AeroDeskPro/src/com/gatorsoft/aerodeskpro/database/DatabaseConnection.java
@@ -1,14 +1,42 @@
 package com.gatorsoft.aerodeskpro.database;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
 
-/**
- *
- * @author User
- */
 public class DatabaseConnection {
-    
+    private static Connection connection;
+    private static final String PROPERTIES_FILE = "db.properties";
+
+    private DatabaseConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try (InputStream input = DatabaseConnection.class.getResourceAsStream(PROPERTIES_FILE)) {
+                Properties props = new Properties();
+                props.load(input);
+
+                String url = props.getProperty("db.url");
+                String username = props.getProperty("db.username");
+                String password = props.getProperty("db.password");
+
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                throw new SQLException("Failed to create DB connection", e);
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
