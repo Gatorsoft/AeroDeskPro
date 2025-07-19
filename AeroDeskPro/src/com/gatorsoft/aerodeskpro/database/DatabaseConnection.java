b5e2aa8f60287966ed411cb6a7f028bb3a4f38dd
Mavros-Lykos <amarasekaracharaka2@gmail.com>
2025-07-19 12:55:50 +0530

    Database Folder for Database connection

---
+++ AeroDeskPro/src/com/gatorsoft/aerodeskpro/database/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package com.gatorsoft.aerodeskpro.database;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class DatabaseConnection {
+    
+}
